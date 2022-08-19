7eaf7b202e94a7d2d8a9e1c5aa79e20c7075c97f
Hyper <rjfance@gmail.com>
2022-08-18 23:35:22 -0400

    :)

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+version '0.0'
+author 'Project Sloth'
+description 'https://github.com/Project-Sloth'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts{
+    'client/*.lua',
+} 
+server_script {
+    'server/*.lua',
+}
